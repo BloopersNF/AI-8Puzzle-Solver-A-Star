00e9d5eddd0ef77e89d3a5a8649141546b50f943
Álex Allan <88742018+BloopersNF@users.noreply.github.com>
2025-03-04 23:18:14 +0000

    AStar Function created, hamming heuristics implemented

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from queue import PriorityQueue
 from scipy.signal import convolve2d as conv2
-
+from time import sleep
 
 class State:
     def __init__(self, parent=None, matrix=None):
@@ -39,14 +39,63 @@ def plays(state):
 
     return state.matrix[np.where(conv)]
 
-matriz1 = State(matrix=np.array([[1,2,3],[4,5,6],[7,8,9]]))
-matriz2 = State(matrix=np.array([[1,2,3],[4,5,6],[7,9,8]]))
+def AStar(state, goal, heuristic):
 
-print(matriz1.matrix == 3)
-print(matriz1.matrix == 0)
-print(matriz1.matrix == 9)
+    cs = set()
 
-matriz2.show()
+    pq = PriorityQueue()
+    state.f = 0
+    state.parent = None
 
-matriz2 = play(matriz2, 8)
-matriz2.show()
+    pq.put((state.f, state))
+
+    while pq is not pq.empty():
+        s = pq.get()[1]
+
+        if s == goal:
+            return s
+        
+        if tuple(s.matrix.flatten()) in cs:
+            continue
+
+        cs.add(tuple(s.matrix.flatten()))
+
+        for p in plays(s):
+            ns = play(s, p)
+            ns.g = s.g + 1
+            ns.parent = s
+            ns.f = ns.g + heuristic(ns)
+            pq.put((ns.f, ns))
+    return state
+
+
+def hamming(state):
+        goal = np.array([[1,2,3],[4,5,6],[7,8,9]])
+        out = len(state.matrix[np.where(state.matrix != goal)]) - 1
+        return max(out, 0)
+
+def manhattan(state):
+    return 0
+
+
+def reconstruct(state):
+    if state.parent is not None:
+        reconstruct(state.parent)
+    print(state.matrix)
+
+
+goal = State(matrix=np.array([[1,2,3],[4,5,6],[7,8,9]]))
+matriz = State(matrix=np.array([[4,6,7],[9,5,8],[2,1,3]]))
+
+result = AStar(matriz, goal, hamming)
+
+print("matriz inicial:")
+print(matriz.matrix)
+print('Encontrando solução...')
+print()
+
+reconstruct(result)
+print(f'Objetivo encontrado em {result.g} movimentos')
+
+
+
